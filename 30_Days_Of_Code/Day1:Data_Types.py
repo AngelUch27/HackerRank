5f900e448341ca1b9522431294a0865afcf41380
Angel Alcántara <115179619+AngelUch27@users.noreply.github.com>
2025-12-18 10:12:02 -0600

    Add data type handling in Day1:Data_Types.py
    Implemented input handling for integer, double, and string variables.

---
+++ 30_Days_Of_Code/Day1:Data_Types.py
@@ -0,0 +1,19 @@
+#https://www.hackerrank.com/challenges/30-data-types/problem?isFullScreen=true
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+entero = 0
+doble = 0.0
+cadena = ""
+# Read and save an integer, double, and String to your variables.
+entero =int(input())
+doble = float(input())
+cadena = str(input())
+# Print the sum of both integer variables on a new line.
+print(i+entero)
+# Print the sum of the double variables on a new line.
+print(d+doble)
+# Concatenate and print the String variables on a new line
+print(s+ cadena)
+# The 's' variable above should be printed first.
