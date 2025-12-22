f61cd3d1b083f9e164b3cdf3e2bab56ae8a6e7c8
Angel Alcántara <115179619+AngelUch27@users.noreply.github.com>
2025-12-22 10:16:22 -0600

    Implement meal cost calculation function

---
+++ 30_Days_Of_Code/2Operators.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+#https://www.hackerrank.com/challenges/30-operators/problem?isFullScreen=true
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'solve' function below.
+#
+# The function accepts following parameters:
+#  1. DOUBLE meal_cost
+#  2. INTEGER tip_percent
+#  3. INTEGER tax_percent
+#
+
+def solve(meal_cost, tip_percent, tax_percent):
+    # Write your code here
+    propa = meal_cost*tip_percent/100
+    palgobierno = meal_cost*tax_percent/100
+    print(round(meal_cost+propa+palgobierno))
+    
+
+if __name__ == '__main__':
+    meal_cost = float(input().strip())
+
+    tip_percent = int(input().strip())
+
+    tax_percent = int(input().strip())
+
+    solve(meal_cost, tip_percent, tax_percent)
